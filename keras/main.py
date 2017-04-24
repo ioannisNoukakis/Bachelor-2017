948348612f689b8c4b7092d0e3c06c831e930e79
ioannis <ioannis.noukakis@heig-vd.ch>
2017-04-25 00:35:35 +0200

    Added deconv tool to analisys

--- keras/main.py
+++ keras/main.py
@@ -2,9 +2,8 @@ from keras.models import Sequential
 from keras.layers import *
 from keras.utils import np_utils
 from imgUtils import *
-from mailsUtils import send_mail
+from quiver_engine import server
 import time
-import sys
 
 import pandas as pd
 # https://elitedatascience.com/keras-tutorial-deep-learning-in-python#step-1
@@ -17,7 +16,7 @@ def main():
     start = time.strftime("%c")
     theTrueScore = []
     nb_classes = imgU.discover_and_make_order()
-    N_EPOCHS = 5
+    N_EPOCHS = 1
 
     # Define model architecture
     model = Sequential()
@@ -38,19 +37,20 @@ def main():
 
     # Train
     redo = True
-    for i in range(0, N_EPOCHS):
-        while redo:
-            redo, x_train, y_train = imgU.load_dataset()
-            # Preprocessing
-            x_train = x_train.astype('float32')
-            x_train /= 255
+    # for i in range(0, N_EPOCHS):
+    while redo:
+        redo, x_train, y_train = imgU.load_dataset()
+        # Preprocessing
+        x_train = x_train.astype('float32')
+        x_train /= 255
 
-            y_train = np_utils.to_categorical(y_train, nb_classes)
+        y_train = np_utils.to_categorical(y_train, nb_classes)
 
-            # Fit model on training data
-            print("Starting...")
-            model.fit(x_train, y_train, batch_size=32, nb_epoch=1, verbose=1)
-            # TODO: Maybe this is the wrong order of how to apply epochs -> investigate
+        # Fit model on training data
+        print("Starting...")
+        model.fit(x_train, y_train, batch_size=32, nb_epoch=N_EPOCHS, verbose=1)
+        # TODO: Maybe this is the wrong order of how to apply epochs -> investigate
+        # redo = True
 
     print("Train completed! Will now evalutate...")
 
@@ -66,12 +66,14 @@ def main():
         # Evaluate model on test data
         theTrueScore.append(model.evaluate(x_test, y_test_2, batch_size=32, verbose=1))
 
-        y_hat = model.predict_classes(x_test)
-        print(pd.crosstab(y_hat, y_test))
+        # y_hat = model.predict_classes(x_test)
+        # print(pd.crosstab(y_hat, y_test))
+
+    server.launch(model, temp_folder='./tmp', input_folder='./visual',  port=5000)
 
     # Log
     # with open('log.txt', 'w') as f:
-     #   sys.stdout = f
+    #   sys.stdout = f
     print("Model:")
     model.summary()
     print("Obtained the score:", theTrueScore)
@@ -79,11 +81,11 @@ def main():
     print("Training ended at:", time.strftime("%c"))
     print("Classes:", nb_classes)
     print("Nb_epoch:", 10)
-        #sys.stdout = sys.__stdout__
+    # sys.stdout = sys.__stdout__
 
-    #with open('log.txt', 'r') as f:
+    # with open('log.txt', 'r') as f:
     #    send_mail("ioannisbachelorbot@gmail.com", "inoukakis@gmail.com", "HEIG-VDkeras2017", f.read())
-        # send_mail("ioannisbachelorbot@gmail.com", "inoukakis@gmail.com", "<mdp>", f.read())
+    # send_mail("ioannisbachelorbot@gmail.com", "inoukakis@gmail.com", "<mdp>", f.read())
 
 if __name__ == "__main__":
     main()
