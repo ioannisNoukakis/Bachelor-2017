78cd88e2b7d816093a7f8e7b419064fd75e0888c
Ioannis Noukakis <ioannis.noukakis@heig-vd.ch>
2017-06-20 11:04:39 +0200

    updated deployment

--- keras/make_env.sh
+++ keras/make_env.sh
@@ -2,4 +2,9 @@
 apt-get update && apt-get upgrade -y
 apt-get install gcc
 apt-get install git
+apt-get install make
 apt-get install linux-headers-$(uname -r)
+cd $HOME
+wget http://fr.download.nvidia.com/XFree86/Linux-x86_64/367.57/NVIDIA-Linux-x86_64-367.57.run
+chmod +x NVIDIA-Linux*
+./NVIDIA-Linux*
