ce1c191b28d5bd748e9c4b019006cbc78e023a07
Ioannis Noukakis <ioannis.noukakis@heig-vd.ch>
2017-06-29 10:14:07 +0200

    changed method to save results to csv

--- keras/bias_metric.py
+++ keras/bias_metric.py
@@ -27,7 +27,7 @@ def save_to_csv(l, e):
 
     with open('results.csv', 'a') as f:
         writer = csv.writer(f)
-        writer.writerows([l, e])
+        writer.writerow([l, e])
         f.close()
 
 
