af19ee9da906a28ddf20f643dbb5ce1e041e5210
Ioannis Noukakis <ioannis.noukakis@heig-vd.ch>
2017-07-14 11:17:40 +0200

    added cython to heatmap_generate

--- keras/setup.py
+++ keras/setup.py
@@ -2,6 +2,6 @@ from distutils.core import setup
 from Cython.Build import cythonize
 
 setup(
-    requires=['PIL', 'keras', 'quiver_engine', 'sklearn', 'cv2',
+    ext_modules=cythonize("heatmapgenerate.pyx"), requires=['PIL', 'keras', 'quiver_engine', 'sklearn', 'cv2',
                                                              'matplotlib', 'h5py', 'Cython']
 )
